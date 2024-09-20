94e21cda4e1cd8ac9b4a434bbe4017b6348f3cb7
Harikrishnan K S <101123232+hari04204@users.noreply.github.com>
2024-09-20 12:20:34 +0530

    Time: 2 ms (96.83%) | Memory: 46.9 MB (12.96%) - LeetSync

--- 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.java
+++ 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.java
@@ -5,7 +5,9 @@ class Solution {
         for(int i = arr.length-2; i>=0; i--){
             int temp = arr[i];
             arr[i] = max;
-            max = Math.max(max,temp);
+            if(temp>max) {
+                max = temp;
+            }
         }
         return arr;
     }
