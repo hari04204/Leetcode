72c39b2a3d7e0b7640b342648ac58a96d171eab2
Harikrishnan K S <101123232+hari04204@users.noreply.github.com>
2024-09-28 18:09:18 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (40.55%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -1,32 +1,57 @@
 class Solution {
     public boolean search(int[] arr, int target) {
-        int start = 0;
-        int end = arr.length-1;
-        while(start<=end){
-            int mid = start + (end - start)/2;
-            System.out.println(mid);
-            if(arr[mid] == target) return true;
-            if(arr[start]==arr[mid] && arr[mid] == arr[end]){
-                start++;
-                end--;
-            }
-            else if(arr[start]<=arr[mid]){
-                if(arr[start] <= target && arr[mid] >= target){
-                    end = mid-1;
-                }
-                else{
-                    start = mid+1;
-                }
+        return search2(arr,target,0,arr.length-1);
+        // int start = 0;
+        // int end = arr.length-1;
+        // while(start<=end){
+        //     int mid = start + (end - start)/2;
+        //     System.out.println(mid);
+        //     if(arr[mid] == target) return true;
+        //     if(arr[start]==arr[mid] && arr[mid] == arr[end]){
+        //         start++;
+        //         end--;
+        //     }
+        //     else if(arr[start]<=arr[mid]){
+        //         if(arr[start] <= target && arr[mid] >= target){
+        //             end = mid-1;
+        //         }
+        //         else{
+        //             start = mid+1;
+        //         }
+        //     }
+        //     else{
+        //         if(target >= arr[mid] && target <= arr[end]){
+        //             start = mid+1;
+        //         }
+        //         else{
+        //             end = mid-1;
+        //         }
+        //     }
+        // }
+        // return false;
+    }
+    public boolean search2(int[] arr, int target, int start, int end){
+        if(start>end) return false;
+        int mid =(start+end)/2;
+        if(arr[mid]== target) return true;
+        if(arr[start]==arr[mid] && arr[mid] == arr[end]){
+                return search2(arr, target, start+1, end-1);
+        }
+        else if(arr[start]<=arr[mid]){
+            if(arr[start] <= target && arr[mid] >= target){
+                return search2(arr, target, start, mid-1);
             }
             else{
-                if(target >= arr[mid] && target <= arr[end]){
-                    start = mid+1;
-                }
-                else{
-                    end = mid-1;
-                }
+                return search2(arr, target, mid+1, end);
             }
         }
-        return false;
+        else{
+            if(target >= arr[mid] && target <= arr[end]){
+                return search2(arr, target, mid+1, end);
+            }
+            else{
+                return search2(arr, target, start, mid-1);
+            }
+        }
     }
 }
