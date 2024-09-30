8e85c8ff9307ae876404be715e7547f9c1edba03
Harikrishnan K S <101123232+hari04204@users.noreply.github.com>
2024-09-30 20:45:19 +0530

    Time: 11 ms (44.87%) | Memory: 47.5 MB (72.26%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -4,8 +4,7 @@ class Solution {
         ArrayList<Integer> list = new ArrayList<>();
         HashMap<Integer,Integer> map = new HashMap<>();
         for(int i:nums){
-            if(map.containsKey(i)) map.put(i,map.get(i)+1);
-            else map.put(i,1);
+            map.put(i, map.getOrDefault(i, 0) + 1);
         }
         for(Map.Entry<Integer,Integer> entry: map.entrySet()){
             if(entry.getValue()> n/3){
