cd5004701220495d2996f9da0579fb64aeb97252
Harikrishnan K S <101123232+hari04204@users.noreply.github.com>
2025-02-20 19:12:23 +0530

    Time: 160 ms (22.64%), Space: 62.5 MB (93.61%) - LeetHub

--- 2616-maximal-score-after-applying-k-operations/2616-maximal-score-after-applying-k-operations.java
+++ 2616-maximal-score-after-applying-k-operations/2616-maximal-score-after-applying-k-operations.java
@@ -5,11 +5,10 @@ class Solution {
             pq.offer(i);
         }
         long ans = 0;
-        while(k>0){
+        while(k-->0){
             int num = pq.poll();
             ans+=num;
             pq.offer((int)Math.ceil(num/3.0));
-            k--;
         }
         return ans;
     }
