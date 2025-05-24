3144fc0d6d4d6a07a3dff8560dad5d7b759af95c
Harikrishnan K S <101123232+hari04204@users.noreply.github.com>
2025-05-24 12:10:33 +0530

    Time: 5 ms (87.27%), Space: 42.3 MB (72.26%) - LeetHub

--- 0299-bulls-and-cows/0299-bulls-and-cows.java
+++ 0299-bulls-and-cows/0299-bulls-and-cows.java
@@ -5,16 +5,15 @@ class Solution {
         int[] freq1 = new int[10];
         int[] freq2 = new int[10];
         for(int i = 0; i<secret.length(); i++){
-            freq1[(secret.charAt(i)-'0')]++;
-            freq2[(guess.charAt(i)-'0')]++;
-        }
-        for(int i = 0; i<secret.length(); i++){
             if(secret.charAt(i)==guess.charAt(i)){
-                freq2[guess.charAt(i)-'0']--;
-                freq1[guess.charAt(i)-'0']--;
                 bulls++;
             }
+            else{
+                freq1[(secret.charAt(i)-'0')]++;
+                freq2[(guess.charAt(i)-'0')]++;
+            }
         }
+        
         for(int i = 0; i<10; i++){
             if(freq2[i]!=0) cows+= Math.min(freq1[i],freq2[i]);
         }
