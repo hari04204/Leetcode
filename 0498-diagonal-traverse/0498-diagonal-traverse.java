7fc51134637c2b1963a6d2df378fae3e21a9f515
Harikrishnan K S <101123232+hari04204@users.noreply.github.com>
2025-05-26 22:45:48 +0530

    Time: 2 ms (99.4%), Space: 47.1 MB (82.8%) - LeetHub

--- 0498-diagonal-traverse/0498-diagonal-traverse.java
+++ 0498-diagonal-traverse/0498-diagonal-traverse.java
@@ -2,51 +2,37 @@ class Solution {
     public int[] findDiagonalOrder(int[][] mat) {
         int m = mat.length;
         int n = mat[0].length;
-        int[] ans = new int[m*n];
+        int[] result = new int[m * n];
+        int row = 0, col = 0, dir = 1;
         int idx = 0;
-        int i = 0;
-        int j = 0;
-        int flag = 1;
-        while(true){
-            if(flag==1 && i<0 && j==n){
-                i+=2;
-                j=n-1;
-                flag = -1;
-            }
-            else if(flag == -1 && j<0 && i==m){
-                i=m-1;
-                j+=2;
-                flag = 1;
-            }
-            else if(flag==1 && i<0){
-                i=0;
-                flag=-1;
-            }
-            else if(flag==-1 && j<0){
-                j=0;
-                flag = 1;
-            }
-            else if(flag==1 && j==n){
-                j=n-1;
-                i+=2;
-                flag=-1;
-            }
-            else if(flag==-1 && i==m){
-                i=m-1;
-                j+=2;
-                flag = 1;
-            }
-            ans[idx++] = mat[i][j];
-            if(i==m-1 && j==n-1) break;
-            if(flag==1){
-                i--;
-                j++;
-            }
-            else{
-                i++;
-                j--;
+
+        while (idx < m * n) {
+            result[idx++] = mat[row][col];
+            if (dir == 1) {
+                if (col == n - 1) {
+                    row++;
+                    dir = -1;
+                } else if (row == 0) {
+                    col++;
+                    dir = -1;
+                } else {
+                    row--;
+                    col++;
+                }
+            } else {
+                if (row == m - 1) {
+                    col++;
+                    dir = 1;
+                } else if (col == 0) {
+                    row++;
+                    dir = 1;
+                } else {
+                    row++;
+                    col--;
+                }
             }
         }
-        return ans;
+
+        return result;
     }
 }
