6bc71739ba5d67b4a3d7814c11a2cba2ff253c9a
Harikrishnan K S <101123232+hari04204@users.noreply.github.com>
2025-08-04 15:12:13 +0530

    Time: 48 ms (81.14%), Space: 57 MB (7.09%) - LeetHub

---
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int totalFruit(int[] fruits) {
+        int n = fruits.length;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int l = 0, r = 0;
+        int max = 0;
+        while(r<n){
+            map.put(fruits[r],map.getOrDefault(fruits[r],0)+1);
+            while(map.size()>2){
+                map.put(fruits[l], map.get(fruits[l])-1);
+                if(map.get(fruits[l])==0) map.remove(fruits[l]);
+                l++;
+            }
+            max = Math.max(max, r-l+1);
+            r++;
+        }
+        return max;
+    }
+}
