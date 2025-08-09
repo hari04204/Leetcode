e7bc0b7a82cd78a7bf64085ecde6f30ab46e9020
Harikrishnan K S <101123232+hari04204@users.noreply.github.com>
2025-08-09 16:40:47 +0530

    Time: 10 ms (98.74%), Space: 63.9 MB (20.96%) - LeetHub

--- 0403-frog-jump/0403-frog-jump.java
+++ 0403-frog-jump/0403-frog-jump.java
@@ -1,35 +1,30 @@
 class Solution {
     public boolean canCross(int[] stones) {
         int n = stones.length;
-        if (n > 1 && stones[1] != 1) return false; // first jump must be 1
-
-        // Map stone position to index
-        Map<Integer, Integer> posToIndex = new HashMap<>();
-        for (int i = 0; i < n; i++) {
-            posToIndex.put(stones[i], i);
-        }
-
-        // Memoization: dp[index][lastJump]
+        if (n > 1 && stones[1] != 1) return false;
         Boolean[][] dp = new Boolean[n][n + 1];
-        return dfs(0, 0, stones, posToIndex, dp);
+        return dfs(0, 0, stones, dp);
     }
 
-    private boolean dfs(int index, int lastJump, int[] stones,
-                        Map<Integer, Integer> posToIndex, Boolean[][] dp) {
+    private boolean dfs(int index, int lastJump, int[] stones, Boolean[][] dp) {
         if (index == stones.length - 1) return true;
         if (dp[index][lastJump] != null) return dp[index][lastJump];
 
         for (int jump = lastJump - 1; jump <= lastJump + 1; jump++) {
             if (jump > 0) {
                 int nextPos = stones[index] + jump;
-                if (posToIndex.containsKey(nextPos)) {
-                    int nextIndex = posToIndex.get(nextPos);
-                    if (dfs(nextIndex, jump, stones, posToIndex, dp)) {
-                        return dp[index][lastJump] = true;
+                for (int nextIndex = index + 1; nextIndex < stones.length; nextIndex++) {
+                    if (stones[nextIndex] == nextPos) {
+                        if (dfs(nextIndex, jump, stones, dp)) {
+                            return dp[index][lastJump] = true;
+                        }
+                        break; // found the stone; no need to keep scanning further
                     }
+                    if (stones[nextIndex] > nextPos) break; // no match possible past this point
                 }
             }
         }
+
         return dp[index][lastJump] = false;
     }
 }
