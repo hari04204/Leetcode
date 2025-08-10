cfc8ede8602acdd342961db8f5f9dcdb96dd35c2
Harikrishnan K S <101123232+hari04204@users.noreply.github.com>
2025-08-10 20:04:20 +0530

    Time: 9 ms (60.96%), Space: 49.8 MB (13.36%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -2,22 +2,17 @@ class Solution {
     public int change(int amount, int[] coins) {
         int n = coins.length;
         int[][] dp = new int[n][amount+1];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j <= amount; j++) {
-                dp[i][j] = -1;
+        for(int i = 0; i<=amount; i++){
+            if(i%coins[0]==0)dp[0][i]=1;
+        }
+        for(int i = 1; i<n; i++){
+            for(int j = 0; j<=amount; j++){
+                int skip = dp[i-1][j];
+                int take = 0;
+                if(coins[i]<=j) take = dp[i][j-coins[i]];
+                dp[i][j] = take+skip;
             }
         }
-        return helper(n-1, coins, amount,dp);
-    }
-    public int helper(int i, int[] coins, int target, int[][] dp){
-        if(i==0){
-            if(target%coins[i]==0) return 1;
-            else return 0;
-        }
-        if(dp[i][target]!=-1)return dp[i][target];
-        int take = 0;  
-        if(target-coins[i]>=0) take = helper(i,coins,target-coins[i],dp);
-        int skip =  helper(i-1, coins, target,dp);
-        return dp[i][target] = take+skip;
+        return dp[n-1][amount];
     }
 }
