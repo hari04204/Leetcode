3b40cb0a7bb66c70599bb8579ec42042bbeda7b5
Harikrishnan K S <101123232+hari04204@users.noreply.github.com>
2025-08-25 11:32:09 +0530

    Time: 5 ms (60.33%), Space: 45.1 MB (45.91%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,18 +1,36 @@
 class Solution {
     public int minDistance(String s, String t) {
-        int i=s.length();
-        int j=t.length();
-        int[][] dp = new int[i][j];
-        for(int[] row: dp){
-            Arrays.fill(row, -1);
+        int m = s.length();
+        int n = t.length();
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 0; i <= m; i++) {
+            dp[i][0] = i;
         }
-        return helper(s,t,s.length()-1, t.length()-1, dp);
+        for (int i = 0; i <= n; i++) {
+            dp[0][i] = i;
+        }
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (s.charAt(i - 1) == t.charAt(j-1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = 1 + Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1]));
+                }
+            }
+        }
+        return dp[m][n];
     }
-    public int helper(String s, String t, int i, int j, int[][] dp){
-        if(i<0) return j+1;
-        if(j<0) return i+1;
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(s.charAt(i)==t.charAt(j)) return dp[i][j] = helper(s,t,i-1,j-1,dp);
-        return dp[i][j]= 1+ Math.min(helper(s,t,i-1,j-1,dp),Math.min(helper(s,t,i,j-1,dp),helper(s,t,i-1,j,dp)));
+
+    public int helper(String s, String t, int i, int j, int[][] dp) {
+        if (i < 0)
+            return j + 1;
+        if (j < 0)
+            return i + 1;
+        if (dp[i][j] != -1)
+            return dp[i][j];
+        if (s.charAt(i) == t.charAt(j))
+            return dp[i][j] = helper(s, t, i - 1, j - 1, dp);
+        return dp[i][j] = 1 + Math.min(helper(s, t, i - 1, j - 1, dp),
+                Math.min(helper(s, t, i, j - 1, dp), helper(s, t, i - 1, j, dp)));
     }
 }
