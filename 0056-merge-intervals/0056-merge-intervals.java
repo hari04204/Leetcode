d0b31cc5490b9509f29fbe977431a5c7ab3dff6b
Harikrishnan K S <101123232+hari04204@users.noreply.github.com>
2025-09-02 21:22:15 +0530

    Time: 10 ms (20.79%), Space: 47 MB (16.15%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,50 +1,28 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[0]-b[0]);
-        for(int[] i : intervals){
-            pq.add(i);
-        }
-        List<int[]> lst = new ArrayList<>();
-        while(pq.size()>1){
-            int[] a = pq.poll();
-            int[] b = pq.poll();
-            if(a[1]>=b[0]){
-                pq.add(new int[]{a[0],Math.max(a[1],b[1])});
+        List<int[]> ans = new ArrayList<>();
+        Arrays.sort(intervals, (a,b)->{
+            if(a[0]==b[0]) return a[1]-b[1];
+            return a[0]-b[0];
+        });
+        int curStart = intervals[0][0];
+        int curEnd = intervals[0][1];
+        for(int i=1; i<intervals.length; i++){
+            if(intervals[i][0]>curEnd){
+                ans.add(new int[]{curStart, curEnd});
+                curStart = intervals[i][0];
+                curEnd = intervals[i][1];
             }
-            else{
-                lst.add(a);
-                pq.add(b);
+            else {
+                curEnd = Math.max(curEnd, intervals[i][1]);
             }
         }
-        lst.add(pq.poll());
-        int[][] ans = new int[lst.size()][2];
-        int x = 0;
-        for(int[] i: lst){
-            ans[x][0]=i[0];
-            ans[x][1]=i[1];
-            x++;
+        ans.add(new int[]{curStart, curEnd});
+        int[][] res = new int[ans.size()][2];
+        for(int i = 0; i<ans.size(); i++){
+            res[i][0] = ans.get(i)[0];
+            res[i][1] = ans.get(i)[1];
         }
-        return ans;
+        return res;
     }
-    // public int[][] merge(int[][] intervals) {
-    //     Arrays.sort(intervals, (a,b)-> {
-    //         if(a[0]==b[0]) return a[1]-b[1];
-    //         else return a[0]-b[0];
-    //     });
-        
-    //     List<int[]> lst = new ArrayList<>();
-    //     int[] curr = intervals[0];
-        
-    //     for(int i = 1; i<intervals.length; i++){
-    //         if(curr[1]>=intervals[i][0]){
-    //             curr[1] = Math.max(curr[1],intervals[i][1]);
-    //         }
-    //         else{
-    //             lst.add(curr);
-    //             curr = intervals[i];
-    //         }
-    //     }
-    //     lst.add(curr);
-    //     return lst.toArray(new int[lst.size()][]);
-    // }
 }
