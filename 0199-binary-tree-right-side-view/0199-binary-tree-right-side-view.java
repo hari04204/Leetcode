f54f1359e270c26131b877e12d06e91bc1cca8a9
Harikrishnan K S <101123232+hari04204@users.noreply.github.com>
2025-09-06 21:56:54 +0530

    Time: 1 ms (70.84%), Space: 42.2 MB (44.08%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -13,23 +13,41 @@
  *     }
  * }
  */
-class Solution {
-    public List<Integer> rightSideView(TreeNode root) {
+// class Solution {
+//     public List<Integer> rightSideView(TreeNode root) {
+//         if(root==null) return new ArrayList<>();
+//         Queue<TreeNode> q = new LinkedList<>();
+//         q.add(root);
+//         List<Integer> ans = new ArrayList();
+//         while(!q.isEmpty()){
+//             int s = q.size();
+//             for(int i=0; i<s; i++){
+//                 TreeNode node = q.remove();
+//                 if(node.left!=null) q.add(node.left);
+//                 if(node.right!=null) q.add(node.right);
+//                 if(i==s-1){
+//                     ans.add(node.val);
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
+// }
+class Solution{
+    public List<Integer> rightSideView(TreeNode root){
         if(root==null) return new ArrayList<>();
-        Queue<TreeNode> q = new LinkedList<>();
-        q.add(root);
-        List<Integer> ans = new ArrayList();
-        while(!q.isEmpty()){
-            int s = q.size();
-            for(int i=0; i<s; i++){
-                TreeNode node = q.remove();
-                if(node.left!=null) q.add(node.left);
-                if(node.right!=null) q.add(node.right);
-                if(i==s-1){
-                    ans.add(node.val);
-                }
-            }
-        }
+        Set<Integer> levels = new HashSet<>();
+        List<Integer> ans = new ArrayList<>();
+        dfs(root,levels,0,ans);
         return ans;
+    }
+    public void dfs(TreeNode node, Set<Integer> levels, int lvl, List<Integer> ans){
+        if(node==null) return;
+        if(!levels.contains(lvl)){
+            ans.add(node.val);
+            levels.add(lvl);
+        }
+        dfs(node.right,levels,lvl+1,ans);
+        dfs(node.left,levels,lvl+1,ans);
     }
 }
