db12941cf49cf54b4ce442891a172722c4a5742b
Harikrishnan K S <101123232+hari04204@users.noreply.github.com>
2025-09-08 10:35:03 +0530

    Time: 1 ms (99.62%), Space: 44.5 MB (55.93%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -4,37 +4,38 @@ class Solution {
         for(int i = 0; i<9; i++){
             for(int j = 0; j<9; j++){
                 if(board[i][j]!='.' && f1[board[i][j]-'0']) return false;
-                if(board[i][j]!='.') f1[board[i][j]-'0'] = true;
+                if(board[i][j]!='.') f1[board[i][j]-'0']=true;
             }
-            f1 = new boolean[10];
+            f1=new boolean[10];
         }
+        f1=new boolean[10];
         for(int i = 0; i<9; i++){
             for(int j = 0; j<9; j++){
                 if(board[j][i]!='.' && f1[board[j][i]-'0']) return false;
-                if(board[j][i]!='.') f1[board[j][i]-'0'] = true;
+                if(board[j][i]!='.') f1[board[j][i]-'0']=true;
             }
-            f1 = new boolean[10];
+            f1=new boolean[10];
         }
         boolean[] f2 = new boolean[10];
         boolean[] f3 = new boolean[10];
-        for(int i = 0; i<9; i++){
-            if(i==3||i==6){
-                f1 = new boolean[10];
-                f2 = new boolean[10];
-                f3 = new boolean[10];
+        for(int i=0; i<9; i++){
+            if(i==3 || i==6){
+                f1=new boolean[10];
+                f2=new boolean[10];
+                f3=new boolean[10];
             }
-            for(int j = 0; j<9; j++){
-                if(j<3){
-                    if(board[i][j]!='.' && f1[board[i][j]-'0']) return false;
-                    if(board[i][j]!='.') f1[board[i][j]-'0'] = true;
+            for(int j=0; j<9; j++){
+                if(j<3 && board[i][j]!='.'){
+                    if(f1[board[i][j]-'0']) return false;
+                    f1[board[i][j]-'0']=true;
                 }
-                else if(j<6){
-                    if(board[i][j]!='.' && f2[board[i][j]-'0']) return false;
-                    if(board[i][j]!='.') f2[board[i][j]-'0'] = true;
+                else if(j<6 && board[i][j]!='.'){
+                    if(f2[board[i][j]-'0']) return false;
+                    f2[board[i][j]-'0']=true;
                 }
-                else{
-                    if(board[i][j]!='.' && f3[board[i][j]-'0']) return false;
-                    if(board[i][j]!='.') f3[board[i][j]-'0'] = true;
+                else if(j<9 && board[i][j]!='.'){
+                    if(f3[board[i][j]-'0']) return false;
+                    f3[board[i][j]-'0']=true;
                 }
             }
         }
