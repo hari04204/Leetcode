8c6a60239303bc1de0a2d166105eddf0941c6d27
Harikrishnan K S <101123232+hari04204@users.noreply.github.com>
2025-10-31 22:00:11 +0530

    Time: 112 ms (5.02%), Space: 63.7 MB (5.03%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -12,22 +12,20 @@ class Solution {
     //     return false;
     // }
     public boolean isMatch(String s, String p){
-        int m = s.length();
-        int n = p.length();
+        int m = s.length(), n = p.length();
         Boolean[][] dp = new Boolean[m+1][n+1];
         return helper(s,p,0,0,dp);
     }
     public boolean helper(String s, String p, int i, int j, Boolean[][] dp){
         if(i==s.length() && j==p.length()) return true;
+        if(dp[i][j]!=null) return dp[i][j];
         if(i==s.length()){
             while(j<p.length() && p.charAt(j)=='*') j++;
             return j==p.length();
         }
-        else if(i==s.length() || j==p.length()) return false;
-        if(dp[i][j]!=null) return dp[i][j];
-        if(s.charAt(i)==p.charAt(j)) return dp[i][j]=helper(s,p,i+1,j+1,dp);
+        if(j==p.length()) return dp[i][j]=false;
+        if(s.charAt(i)==p.charAt(j) || p.charAt(j)=='?') return dp[i][j]=helper(s,p,i+1,j+1,dp);
         if(p.charAt(j)=='*') return dp[i][j]=helper(s,p,i+1,j+1,dp) || helper(s,p,i+1,j,dp) || helper(s,p,i,j+1,dp);
-        if(p.charAt(j)=='?') return dp[i][j]=helper(s,p,i+1,j+1,dp);
         return dp[i][j]=false;
     }
 }
